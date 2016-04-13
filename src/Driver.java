7a28902de9298a12d1311a609b61d5849e3062e6
Abdullah Alshammari <Abdull@ashlis-iphone.reshall>
2016-04-13 01:37:11 -0500

    first commit

--- src/Driver.java
+++ src/Driver.java
@@ -3,43 +3,31 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		String s = "hello";
-		String c = "he11o";
-		Driver.removeVowels(s); 
-		Driver.removeConsonants(s); 
-		Driver.removeDigits(c);
+		String s = "124";
+		String c = "123";
+		Driver.stringToInt(s);
+
 
 	}
+
+
+     //return the integer version of the char parameter
+       static int charToInt(char c)
+        {
+	     return "0123456789".indexOf(c);
+  }
+
+       //Converts s, which is a string representation
+       //of an int into an int representation
+       //"124" -> 124
+       static int stringToInt(String s)
+{
+    	   s = s = s;
+    	   int i = Integer.valueOf(s);
+    	   System.out.println(s);
+		return i; 
+    	}
 	
-	//returns a new String with all of the vowels
-	//removed form the parameter String
-	//"hello" -> hll
-	static String removeVowels(String s)
-	{
-		 s = s .replaceAll("[aeiou]","");
-		 System.out.println(s);
-		return "s";
-	}
-	
-	//returns a new String with all of the consonants
-	//removed form the parameter String
-	//"hello" -> eo
-	static String removeConsonants(String s)
-	{
-		s = s .replaceAll("[bcdfghjklmnpqrst]","");
-		System.out.println(s);
-		return "s";
-	}
-		
-	//returns a new String with all of the numbers
-	//removed form the parameter String
-	//"he11o" -> heo
-	static String removeDigits(String c)
-	{
-		c = c .replaceAll("[0123456789]","");
-		System.out.println(c);
-		return "c";
-	}
 		
 	//return the first occurence in s where c is found or -1 if 
 	//it was not found - we can't use the String's indexOf
